95e0723d98e6c1c2880baa02f4549467c9e3a0c7
brenoborba <brenoborba99@gmail.com>
2022-03-01 21:26:41 +0100

    Adding the function of 'task completed'.

--- js/main.js
+++ js/main.js
@@ -6,21 +6,29 @@ const Main = {
     },
 
     cacheSelectors: function(){ /* Seleciona e armazena elementos do HTML. */
-        this.checkButtons = document.querySelectorAll('.check')
+        this.$checkButtons = document.querySelectorAll('.check')
     },
 
     bindEvents: function(){ 
         const self = this
 
-        this.checkButtons.forEach(function(button){
+        this.$checkButtons.forEach(function(button){
             button.onclick = self.Events.checkButtons_click
         })
     },
 
 
     Events: {
-        checkButtons_click: function(){
-            alert('ok')
+        checkButtons_click: function(e){
+            const li = e.target.parentElement
+            const isDone = li.classList.contains('done')
+
+            if(!isDone){
+                return li.classList.add('done')
+            }
+            
+            li.classList.remove('done')
+            
         }
 
     }
